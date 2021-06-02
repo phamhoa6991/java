3ba0a76030f88db64047609342cc65d700684c4f
phamhoa6991 <82876671+phamhoa6991@users.noreply.github.com>
2021-06-02 20:54:24 +0700

    java

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useState,useEffect} from 'react'
+function App() {
+   }
+   
+    
+ 
+
+  return (
+    <div>
+      <input placeholder="name"/>
+    </div>);
+}
+
+export default App;
